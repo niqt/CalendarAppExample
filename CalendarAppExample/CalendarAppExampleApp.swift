613c55543a943a729a0521279a7e90dd589ba454
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-05-08 20:52:41 +0200

    Initial Commit

---
+++ CalendarAppExample/CalendarAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppExampleApp.swift
+//  CalendarAppExample
+//
+//  Created by Nicola De Filippo on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAppExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalendarAppExample
+//
+//  Created by Nicola De Filippo on 08/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
